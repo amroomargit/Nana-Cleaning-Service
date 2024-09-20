9950084d4fff13639f59cde9a3dfd1b2242518ce
Amro Omar <amroomargit@gmail.com>
2024-09-20 00:10:12 -0400

    Fixed issues with the visuals of the mobile dropdown menu

--- header_footer/nav_insertion.js
+++ header_footer/nav_insertion.js
@@ -2,6 +2,24 @@ fetch('../header_footer/navbar.html')
 .then(response => response.text())
 .then(data => {
     document.getElementById('navbar').innerHTML = data;
+
+    //targets id and class created in index.html so that we can add javascript functionality to it
+    const menu = document.querySelector('#mobile-menu')
+    const menuLinks = document.querySelector('.navbar__menu')
+    const navbar = document.querySelector('.navbar');
+
+    //in the event someone clicks on the dropdown menu, this is the functionality we're adding
+    menu.addEventListener('click',function(){
+
+    //toggles on the is-active css class once the click mentioned above happens
+    menu.classList.toggle('is-active');
+    //toggles on the active css class once the click mentioned above happens
+    menuLinks.classList.toggle('active');
+    //toggles on the active css class for navbar once the click mentioned above happens
+    navbar.classList.toggle('active');
+
+});
+
 })
 .catch(error => console.error('Issue loading up the navbar:', error));
 

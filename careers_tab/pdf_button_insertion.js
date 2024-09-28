70c283bb521ec2e10716bdad77ad74108088d32f
Amro Omar <amroomargit@gmail.com>
2024-09-28 01:42:47 -0400

    Fixed styling issues on the carrers page and continued working on pdf submission functionality

--- careers_tab/pdf_button_insertion.js
+++ careers_tab/pdf_button_insertion.js
@@ -1,29 +1,23 @@
-document.getElementById('pdf_file').addEventListener('change',function(){
-    const file = this.files[0];
-    const error_msg = document.getElementById('incorrect_file');
-
-    if(file && file.type !== 'application/pdf'){
-        error_msg.style.display = 'block';
-    }
-    else{
-        error_msg.style.display = 'none';
-    }
-});
-
 document.querySelector('.upload_button').addEventListener('click', function(){
-    document.getElementById('pdf_file').click();
+    const file_Input = document.getElementById('pdf_File');
+
+    this.disabled = true;
+
+    file_Input.click();
+
+    file_Input.addEventListener('change', () =>{
+        this.disabled = false;
+    });
 });
 
-document.getElementById('pdfForm').addEventListener('submit', function(event){
+document.getElementById('pdf_Form').addEventListener('submit', function(event){
+    event.preventDefault();
+
     const form_Data = new FormData();
-    const file_Input = document.getElementById('pdf_file');
+    const file_Input = document.getElementById('pdf_File');
     const file = file_Input.files[0];
-    const error_msg = document.getElementById('file_error')
 
-    if(file && file.type === 'application/pdf'){
-        error_msg.style.display = "none";
-
-        form_Data.append('pdf_file', file);
+        form_Data.append('pdf_File', file);
 
         fetch('/upload',{
             method: 'POST',
@@ -32,16 +26,11 @@ document.getElementById('pdfForm').addEventListener('submit', function(event){
 
         .then(response => response.json())
         .then (data => {
-            console.log('Success:',data);
+            console.log('Success:', data);
         })
 
         .catch(error => {
             console.error('Error:', error);
  
         });
-
-    } else {
-        error_msg.style.display = 'block';
-    }
-
 });

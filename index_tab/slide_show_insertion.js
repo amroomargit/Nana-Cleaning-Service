68d36f06274f954942833cd22b1934d93014961e
Amro Omar <amroomargit@gmail.com>
2024-10-09 00:22:31 -0400

    Fix screen minimization for services tab, image slider on index tab, and customer testimony stylization choices

--- index_tab/slide_show_insertion.js
+++ index_tab/slide_show_insertion.js
@@ -1,8 +1,26 @@
 var counter = 1;
-          setInterval(function(){
-            document.getElementById('radio' + counter).checked = true;
-            counter++;
-            if(counter>4){
-              counter = 1;
-            }
-          }, 5000);
+var interval;
+
+function startAutoSlider() {
+  interval = setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 4){
+      counter = 1;
+    }
+  }, 5000);
+}
+
+function manualSlide(dotNumber) {
+  clearInterval(interval); 
+  counter = dotNumber; 
+  document.getElementById('radio' + dotNumber).checked = true;
+  startAutoSlider(); 
+}
+
+startAutoSlider();
+
+document.getElementById('radio1').addEventListener('click', function() { manualSlide(1); });
+document.getElementById('radio2').addEventListener('click', function() { manualSlide(2); });
+document.getElementById('radio3').addEventListener('click', function() { manualSlide(3); });
+document.getElementById('radio4').addEventListener('click', function() { manualSlide(4); });

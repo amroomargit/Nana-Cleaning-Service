086f1484e78b2861df67a43f1d9d1504871135d7
Amro Omar <amroomargit@gmail.com>
2024-10-11 20:09:15 -0400

    Test deployment database issues solved

--- app.js
+++ app.js
@@ -3,6 +3,34 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const fetch = require('node-fetch');
 const bodyParser = require('body-parser');
+const sql = require('mssql');
+
+//SQL Database configuration
+const sqlConfig = {
+    user: 'aomar58@uwo.ca',
+    password: 'MoZfOt213!',
+    database: 'NanaCleaningServicesLTD_db',
+    server: 'nanacleaningservicesltd.database.windows.net',
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+    options: {
+        encrypt: true,
+        trustServerCertificate: false,
+    }
+};
+
+// Connect to the SQL database
+sql.connect(sqlConfig)
+    .then(pool => {
+        console.log('Connected to SQL Database');
+        return pool;
+    })
+    .catch(err => {
+        console.error('Database connection failed: ',err);
+    });
 
 // Middleware to parse request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,10 +59,23 @@ app.post('/upload', (req, res) => {
 });
 
 // Route for form submission
-app.post('/submit-form', (req, res) => {
+app.post('/submit-form', async (req, res) => {
     const { name, email, phone_number, message } = req.body;
-    console.log(`Name: ${name}, Email: ${email}, Phone Number: ${phone_number}, Message: ${message}`);
-    res.status(200).json({message: 'Form submitted successfully!'});
+
+    try{
+        const pool = await sql.connect(sqlConfig);
+        await pool.request()
+        .input('Name', sql.VarChar, name)
+        .input('Email',sql.VarChar, email)
+        .input('PhoneNumber',sql.VarChar,phone_number)
+        .input('Message',sql.VarChar,message)
+        .query('INSERT INTO Submissions (Name, Email, PhoneNumber, Message) VALUES (@Name, @Email,@PhoneNumber,@Message');
+    
+    res.status(200).json({message: 'Form submitted and saved to the database successfully!'});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Database error'});
+    }
 });
 
 // Start the server
